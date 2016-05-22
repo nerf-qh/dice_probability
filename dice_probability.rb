95faac6cb5fc4b9395c7e8315f361eb82a5a9ffa
Oleg Kravtsov <o.api.qx@gmail.com>
2016-05-22 17:16:09 +0300

    Clear comments code

--- dice_probability.rb
+++ dice_probability.rb
@@ -1,8 +1,35 @@
-require "pp"
+require 'pp'
 R = 6
 
+def calc_count(n, m)
+  return 0 if m > n * R || m < 0
+
+  a = Array.new(n - 1)
+  for i in 0..n - 1
+    a[i] = Array.new(R + 1, 0)
+  end
+
+  row = 0
+  next_row = -> { row = row == R ? 0 : row + 1 }
+  for i in 1..m - 1
+    next_row.call
+    a[0][row] = i < 7 ? 1 : 0 # the first dice
+    for j in 1..n - 2
+      a[j][row] = a[j - 1].inject(-a[j - 1][row]) { |sum, k| sum + k }
+    end
+  end
+  next_row.call
+  a[n - 2].inject(-a[n - 2][row]) { |sum, k| sum + k }
+end
+
+def calc(n, m)
+  variants = R**n
+  count = calc_count(n, m)
+  count.to_f / variants
+end
+
 def main
-  fail "Enter n, m" if ARGV.length < 2
+  fail 'Enter n (count), m (sum)' if ARGV.length < 2
 
   n = ARGV[0].to_i
   m = ARGV[1].to_i
@@ -11,59 +38,6 @@ def main
   pp "Sum: #{m}"
   probability = calc(n, m)
   pp "Probability: #{probability}"
-end
-
-def calc_count(n, m)
-  fail "Incorrect sum" if m < 0
-  return 0 if m > n * R
-
-  a = Array.new(n - 1)
-  for i in 0..n-1
-    a[i] = Array.new(R + 1, 0)
-  end
-  # pp a
-  row = R - 1
-  next_row = -> { row = row == R ? 0 : row + 1 }
-  for i in 1..m-1
-    # pp "**********************"
-    # pp "Sum: #{i}"
-    next_row[]
-    # pp "Current row: #{row}"
-    a[0][row] = i < 7 ? 1 : 0 # the first dice
-    for j in 1..(n - 2)
-      # pp "J: #{j}"
-      # pp "Excluded: #{a[j-1][row]}"
-      # pp a[j-1]
-      a[j][row] = a[j-1].inject(-a[j-1][row]){ |sum, k| sum + k }
-      # pp a[j]
-    end
-    # pp "Rez"
-    # pp a
-    # break if i > 5
-  end
-  # pp "Last:"
-  # pp a[n-2]
-
-  b = []
-  for i in 0..n-1
-    b << a[i][row]
-  end
-  # pp b
-  next_row[]
-  ex = a[n - 2][row]
-  # pp "Row: #{row}, exluded: #{ex}"
-  # pp "-----"
-
-  count = a[n - 2].inject(-ex){ |sum, k| sum + k }
-  # pp count
-  count
-end
-
-def calc(n, m)
-  variants = R ** n
-  count = calc_count(n, m)
-  probability = count.to_f / variants
-  probability
 end
 
 main if __FILE__ == $PROGRAM_NAME
